f3e686b042fe9d21ec8920416c35cda18178c7a7
Noetster123 <152698401+Noetster123@users.noreply.github.com>
2023-12-04 21:37:37 +0100

    dag3

--- Nuriel/Advent of Code/2023/Day3/Day3_part1.m
+++ Nuriel/Advent of Code/2023/Day3/Day3_part1.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 
-fid = fopen('D:\Home Projects\GitHub Projects\aoc-2023\Nuriel\Advent of Code\2023\Day3\input.txt','r');
+fid = fopen('D:\Home Projects\GitHub Projects\aoc-2023\Nuriel\Advent of Code\2023\Day3\example1.txt','r');
 format = '%c';
 doc = splitlines(fscanf(fid,format));
 M = char(doc{1:length(doc)});
@@ -11,8 +11,8 @@ Numbers.coordGear = find(~temp & (double(Mt)~=46));
 
 Numbers.coordNumXY(:,2) = floor(Numbers.coordNum/length(M(1,:)))+1;
 Numbers.coordGearXY(:,2) = floor(Numbers.coordGear/length(M(1,:)))+1;
-Numbers.coordNumXY(:,1) = mod(Numbers.coordNum,length(M(1,:)));
-Numbers.coordGearXY(:,1) = mod(Numbers.coordGear,length(M(1,:)));
+Numbers.coordNumXY(:,1) = mod(Numbers.coordNum-1,length(M(1,:)))+1;
+Numbers.coordGearXY(:,1) = mod(Numbers.coordGear-1,length(M(1,:)))+1;
 
 Numbers.coordNumId = ones(length(Numbers.coordNumXY(:,1)),1);
 Numbers.combined(1).Id = Mt(Numbers.coordNum(1));
@@ -35,4 +35,4 @@ list = find(Numbers.coordNumIdIncl == 1);
 for i = 1:length(list)
     numbersFinal(i) = str2num(Numbers.combined(list(i)).Id);
 end
-answer = sum(numbersFinal);
+answer = sum(numbersFinal); %should be 560670
