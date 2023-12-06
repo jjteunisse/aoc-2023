65b27065e20d0538cba7f4435d66e435bba004ad
jsheemskerk <joranheemskerk@proton.me>
2023-12-06 19:34:33 +0100

    day6

--- oran/src/main.rs
+++ oran/src/main.rs
@@ -4,6 +4,8 @@ mod day2a;
 mod day2b;
 mod day3a;
 mod day3b;
+mod day6a;
+mod day6b;
 use std::{env, process::exit};
 
 fn main() {
@@ -21,6 +23,8 @@ fn main() {
         "day2b" => println!("{}", day2b::solve().unwrap()),
         "day3a" => println!("{}", day3a::solve().unwrap()),
         "day3b" => println!("{}", day3b::solve().unwrap()),
+        "day6a" => println!("{}", day6a::solve().unwrap()),
+        "day6b" => println!("{}", day6b::solve().unwrap()),
         _ => println!("unknown name"),
     }
 
