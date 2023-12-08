0b231235c2020d8dfe6729ab0dbc2e01a0cc40f4
Margot Teunisse <Teunisse@Physics.leidenuniv.nl>
2023-12-08 15:07:47 +0100

    Wrote a naive solution for task 2; try to write something more clever later

--- margot/day8.py
+++ margot/day8.py
@@ -1,3 +1,4 @@
+import numpy as np
 import re
 
 name = "input"
@@ -5,7 +6,6 @@ name = "input"
 pattern = re.compile("\w\w\w")
 
 network = {}
-steps = 0
 with open("inputs/day8/{}.txt".format(name)) as file:
     instructions = ""
     line = next(file).rstrip()
@@ -19,11 +19,27 @@ with open("inputs/day8/{}.txt".format(name)) as file:
 
 current_node = 'AAA'
 arrived = False
+steps = 0
 while not arrived:
     for direction in instructions:
         current_node = network[current_node]["LR".index(direction)]
         steps += 1
         if current_node == 'ZZZ':
             arrived = True
+            break
 
-print(steps)
+print("Required steps(task 1):", steps)
+
+current_nodes = [node for node in network if node[2] == 'A']
+
+arrived = False
+steps = 0
+while not arrived:
+    for direction in instructions:
+        current_nodes = [network[node]["LR".index(direction)] for node in current_nodes]
+        steps += 1
+        if all([node[2] == 'Z' for node in current_nodes]):
+            arrived = True
+            break
+
+print("Required steps(task 2):", steps)
