843e796e7180d2fc5f30c43922421a96a6c19ce1
Margot Teunisse <margotteunisse@hotmail.com>
2023-12-09 20:25:39 +0100

    Solved day 8 puzzles

--- margot/day8.py
+++ margot/day8.py
@@ -1,4 +1,6 @@
 import numpy as np
+import networkx as nx
+from matplotlib import pyplot as plt
 import re
 
 name = "input"
@@ -16,30 +18,47 @@ with open("inputs/day8/{}.txt".format(name)) as file:
     for line in file:
         node, left_node, right_node = pattern.findall(line)
         network[node] = (left_node, right_node)
+        
+#Task 1
+if 'AAA' in network:
+    current_node = 'AAA'
+    arrived = False
+    steps = 0
+    while not arrived:
+        for direction in instructions:
+            current_node = network[current_node]["LR".index(direction)]
+            steps += 1
+            if current_node == 'ZZZ':
+                arrived = True
+                break
 
-current_node = 'AAA'
-arrived = False
-steps = 0
-while not arrived:
-    for direction in instructions:
-        current_node = network[current_node]["LR".index(direction)]
-        steps += 1
-        if current_node == 'ZZZ':
-            arrived = True
-            break
-
-print("Required steps(task 1):", steps)
-
-current_nodes = [node for node in network if node[2] == 'A']
-
-arrived = False
-steps = 0
-while not arrived:
-    for direction in instructions:
-        current_nodes = [network[node]["LR".index(direction)] for node in current_nodes]
-        steps += 1
-        if all([node[2] == 'Z' for node in current_nodes]):
-            arrived = True
-            break
-
-print("Required steps(task 2):", steps)
+    print("Required steps(task 1):", steps)
+    
+#Task 2
+#Check assumptions about input
+starting_nodes = (node for node in network if node[2] == 'A')
+periods = []
+for node in starting_nodes:
+    current_node = node
+    arrived = False
+    steps = 0
+    while not arrived:
+        for direction in instructions:
+            current_node = network[current_node]["LR".index(direction)]
+            steps += 1
+            if current_node[2] == 'Z':
+                arrived = True
+                
+                #Check that a 'Z' node is only reached at the end of a set of instructions
+                assert steps%len(instructions) == 0
+                
+                #Check that the output of the 'Z' node is the same as that of the initial 'A' node
+                assert network[current_node]["LR".index(instructions[-1])] == network[current_node]["LR".index(instructions[-1])]
+                
+                periods.append(steps//len(instructions))
+                
+                break
+                
+#If the periods are all prime numbers, then the first number divisible by all periods is their product.
+#Could write a check for this, but don't really need to since I can check by eye.
+print("Required steps (task 2):", np.product(periods, dtype=np.int64)*len(instructions))
