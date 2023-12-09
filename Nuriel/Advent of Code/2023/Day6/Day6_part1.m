e790aecb53264c6ccce43205ac2fdbf4b549b583
Noetster123 <152698401+Noetster123@users.noreply.github.com>
2023-12-09 23:12:39 +0100

    a

---
+++ Nuriel/Advent of Code/2023/Day6/Day6_part1.m
@@ -0,0 +1,13 @@
+clear all; close all; clc;
+
+fid = fopen('D:\Home Projects\GitHub Projects\aoc-2023\Nuriel\Advent of Code\2023\Day6\input.txt','r');
+format = '%c';
+doc = strsplit(fscanf(fid,format),{'\n','Time:','Distance:'});
+input.Tmax = str2num(doc{2});
+input.Dmax = str2num(doc{3});
+input.temp1 = 0.5*input.Tmax;
+epsilon = 1e-10;
+input.SolutionRange(:,1) = floor(input.temp1 + 0.5*sqrt(input.Tmax.*input.Tmax - 4*input.Dmax)-epsilon);
+input.SolutionRange(:,2) = ceil(input.temp1 - 0.5*sqrt(input.Tmax.*input.Tmax - 4*input.Dmax)+epsilon);
+input.Solutions = input.SolutionRange(:,1) - input.SolutionRange(:,2) + 1;
+answer = prod(input.Solutions);
