f0e9f8d776f3b76ed070f4932604e5ecacad3019
Margot Teunisse <margotteunisse@hotmail.com>
2023-12-11 18:45:46 +0100

    Solved day 11 puzzles.

--- margot/day11.py
+++ margot/day11.py
@@ -1,23 +1,32 @@
 import numpy as np
+import sys
 
-name = "input"
+def calculate_sum_distances(skydat, expansion_factor):
+    horizontal_expansion = expansion_factor**np.all(skydat == '.', axis=0)
+    vertical_expansion = expansion_factor**np.all(skydat == '.', axis=1)
 
-skydat = np.array([list(line.rstrip()) for line in open("inputs/day11/{}.txt".format(name))])
+    #Assign numbers to all galaxies
+    mask = (skydat == "#")
+    locs = {identifier: (i, j) for identifier, (i, j) in enumerate(zip(*np.where(mask)))}
 
-#Apply expansion
-expanded_skydat = np.repeat(skydat, repeats=2**np.all(skydat == '.', axis=1), axis=0)
-expanded_skydat = np.repeat(expanded_skydat, repeats=2**np.all(skydat == '.', axis=0), axis=1)
+    #Calculate all distances between galaxies
+    sum_distances = 0
+    pairs = np.triu_indices(len(locs), k=1)
+    for (id1, id2) in zip(*pairs):
+        loc1, loc2 = locs[id1], locs[id2]
+        sum_distances += np.sum(vertical_expansion[min(loc1[0], loc2[0]):max(loc1[0], loc2[0])])
+        sum_distances += np.sum(horizontal_expansion[min(loc1[1], loc2[1]):max(loc1[1], loc2[1])])
+        
+    return sum_distances
 
-#Assign numbers to all galaxies
-mask = (expanded_skydat == "#")
-locs = {identifier: (i, j) for identifier, (i, j) in enumerate(zip(*np.where(mask)))}
+def main():
+    name = "input"
 
-#Calculate all distances between galaxies
-sum_distances = 0
-pairs = np.triu_indices(len(locs), k=1)
-for (id1, id2) in zip(*pairs):
-    loc1, loc2 = locs[id1], locs[id2]
-    sum_distances += abs(loc1[0]-loc2[0]) + abs(loc1[1]-loc2[1])
+    skydat = np.array([list(line.rstrip()) for line in open("inputs/day11/{}.txt".format(name))])
+
+    print("Sum of distances for expansion factor 2 (task 1):", calculate_sum_distances(skydat, 2))
+    print("Sum of distances for expansion factor 1 million (task 2):", calculate_sum_distances(skydat, 1e6))
     
-print("Sum of distances between galaxies (task 1):", sum_distances)
+if __name__ == '__main__':
+    sys.exit(main())
 
