b51641bca37ef60b0c5a236ab244dd753aaf143d
Margot Teunisse <Teunisse@Physics.leidenuniv.nl>
2023-12-15 13:19:09 +0100

    Solved day 14 puzzles (not very optimized)

--- margot/day14.py
+++ margot/day14.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Tuple
 import sys
 
-def tilt(platform, direction:Tuple[int, int]):
+def tilt(platform, direction:Tuple[int, int]) -> None:
     shifted = True
     while shifted:
         shifted = False
@@ -12,15 +12,39 @@ def tilt(platform, direction:Tuple[int, int]):
                     shifted = True
                     platform[i, j], platform[i+direction[0], j+direction[1]] = ".", "O"
 
+def north_load(platform) -> int:
+    return np.sum([platform.shape[0]-i for i in np.where(platform == "O")[0]])
+
 def main():
     name = "input"
 
     platform = np.array([list(line.rstrip()) for line in open("inputs/day14/{}.txt".format(name))])
 
+    history = [platform.copy()]
+
     tilt(platform, direction = (-1, 0))
 
-    north_load = np.sum([platform.shape[0]-i for i in np.where(platform == "O")[0]])
-    print("Load on north side of platform (task 1):", north_load)
+    print("Load on north side of platform (task 1):", north_load(platform))
+
+    #(Task 2) Tilt the platform cyclically until a limit cycle is reached, then determine transient and period.
+    tilt(platform, direction = (0, -1))
+    tilt(platform, direction = (1, 0))
+    tilt(platform, direction = (0, 1))
+
+    while not any([np.all(platform == platform_prev) for platform_prev in history]):
+        history.append(platform.copy())
+        print(len(history))
+        tilt(platform, direction = (-1, 0))
+        tilt(platform, direction = (0, -1))
+        tilt(platform, direction = (1, 0))
+        tilt(platform, direction = (0, 1))
+    transient = np.where([np.all(platform == platform_prev) for platform_prev in history])[0][0]
+    loop = history[transient:]
+    period = len(loop)
+
+    #Determine the state of the system after 1 billion cycles.
+    platform = loop[int((1e9-transient)%period)]
+    print("Load on north side of platform after 1 billion cycles (task 2):", north_load(platform))
 
 if __name__ == "__main__":
     sys.exit(main())
