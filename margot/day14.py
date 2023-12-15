466a076cd847bcea0d511e395b3c91adf5a4afe4
Margot Teunisse <Teunisse@Physics.leidenuniv.nl>
2023-12-15 11:27:56 +0100

    Restructured day 14 code & removed some redundancy

--- margot/day14.py
+++ margot/day14.py
@@ -1,22 +1,26 @@
 import numpy as np
+from typing import Tuple
+import sys
 
-name = "input"
+def tilt(platform, direction:Tuple[int, int]):
+    shifted = True
+    while shifted:
+        shifted = False
+        for (i, j) in zip(*np.where(platform == "O")):
+            if all([i+direction[0] >= 0, i+direction[0] < platform.shape[0], j+direction[1] >= 0, j+direction[1] < platform.shape[1]]):
+                if platform[i+direction[0], j+direction[1]] == ".":
+                    shifted = True
+                    platform[i, j], platform[i+direction[0], j+direction[1]] = ".", "O"
 
-platform = np.array([list(line.rstrip()) for line in open("inputs/day14/{}.txt".format(name))])
+def main():
+    name = "input"
 
-round_rocks = {identifier:(i, j) for identifier, (i, j) in enumerate(zip(*np.where(platform == "O")))}
+    platform = np.array([list(line.rstrip()) for line in open("inputs/day14/{}.txt".format(name))])
 
-shifted = True
-direction = (-1, 0)
-while shifted:
-    shifted = False
-    for identifier in round_rocks:
-        i, j = round_rocks[identifier]
-        if all([i+direction[0] >= 0, i+direction[0] < platform.shape[0], j+direction[1] >= 0, j+direction[1] < platform.shape[1]]):
-            if platform[i+direction[0], j+direction[1]] == ".":
-                shifted = True
-                platform[i, j], platform[i+direction[0], j+direction[1]] = ".", "O"
-                round_rocks[identifier] = (i+direction[0], j+direction[1])
+    tilt(platform, direction = (-1, 0))
 
-north_load = np.sum([platform.shape[0]-round_rocks[identifier][0] for identifier in round_rocks])
-print("Load on north side of platform (task 1):", north_load)
+    north_load = np.sum([platform.shape[0]-i for i in np.where(platform == "O")[0]])
+    print("Load on north side of platform (task 1):", north_load)
+
+if __name__ == "__main__":
+    sys.exit(main())
