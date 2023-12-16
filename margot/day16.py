ff6dd0020a3b1e925dde9967577a46d7f9d5020d
Margot Teunisse <margotteunisse@hotmail.com>
2023-12-16 16:36:23 +0100

    Solved day 16 puzzles.

--- margot/day16.py
+++ margot/day16.py
@@ -30,7 +30,11 @@ class LightContraption():
                 else:
                     beams_new.add((loc_new, direction))
         self.beams = beams_new - self.stale
-            
+        
+    def reset(self):
+        self.beams = set()
+        self.stale = set()
+        self.energized = np.zeros_like(self.grid, dtype=bool)
     
 def main():
     name = "input"
@@ -45,6 +49,46 @@ def main():
         contrpt.update()
     
     print("Number of energized tiles (task 1):", np.sum(contrpt.energized))
+    
+    #Task 2
+    max_energized = 0
+    #Left and right edge tiles
+    for i in np.arange(contrpt.grid.shape[0]):
+        contrpt.reset()
+        contrpt.beams.add(((i, -1), (0, 1)))
+        
+        while any(contrpt.beams):
+            contrpt.update()
+            
+        max_energized = max(max_energized, np.sum(contrpt.energized))
+        
+        contrpt.reset()
+        contrpt.beams.add(((i, contrpt.grid.shape[1]), (0, -1)))
+        
+        while any(contrpt.beams):
+            contrpt.update()
+            
+        max_energized = max(max_energized, np.sum(contrpt.energized))
+        
+    #Top and bottom edge tiles
+    for j in np.arange(contrpt.grid.shape[1]):
+        contrpt.reset()
+        contrpt.beams.add(((-1, j), (1, 0)))
+        
+        while any(contrpt.beams):
+            contrpt.update()
+            
+        max_energized = max(max_energized, np.sum(contrpt.energized))
+        
+        contrpt.reset()
+        contrpt.beams.add(((contrpt.grid.shape[0], j), (-1, 0)))
+        
+        while any(contrpt.beams):
+            contrpt.update()
+            
+        max_energized = max(max_energized, np.sum(contrpt.energized))
+        
+    print("Optimal number of energized tiles (task 2):", np.sum(max_energized))
 
 if __name__ == "__main__":
     sys.exit(main())
