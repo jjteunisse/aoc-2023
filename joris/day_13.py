7bd8384cbf661214d8e4de2e1b34f03f2c240cbd
jjteunisse <joristeunisse@gmail.com>
2023-12-31 10:54:01 +0100

    [d13] goed genoeg

--- joris/day_13.py
+++ joris/day_13.py
@@ -1,51 +1,42 @@
 Matrix = list[list[str]]
 
-def find(matrix: Matrix) -> int:
-	for i in range(len(matrix) // 2, 0, -1):
-		mirror = mirrored(matrix, i)
-		if mirror == matrix[:i * 2]: return i
-
 def mirrored(matrix: Matrix, line: int) -> Matrix:
 	return matrix[:line] + list(reversed(matrix[:line]))
-
-def pprint(matrix: Matrix):
-	for line in matrix: print(line)
 
 def transposed(matrix: Matrix) -> Matrix:
 	return [''.join(x) for x in zip(*matrix)]
 
-def rev(matrix: Matrix) -> Matrix:
-	return list(reversed(matrix))
+def find(matrix: Matrix, n_diffs: int) -> int:
+	for i in range(len(matrix) // 2, 0, -1):
+		original = ''.join(matrix[:i * 2])
+		mirror = ''.join(mirrored(matrix, i))
+		if sum([int(x != y) for x, y in zip(original, mirror)]) == n_diffs:
+			return i
 
-patterns = [
-	x.split('\n')
-	for x in open('inputs/day_13.txt').read().split('\n\n')
-]
+def get_value(matrix: Matrix, n_diffs: int) -> int:
+	h_len = len(matrix[0])
+	v_len = len(matrix)
 
-summary = 0
-for pattern in patterns:
-	h_start = find(pattern)
-	if h_start:
-		summary += 100 * h_start
-		continue
+	h_start = find(matrix, n_diffs)
+	if h_start: return 100 * h_start
 
-	h_end = find(rev(pattern))
-	if h_end:
-		v_len = len(pattern)
-		summary += 100 * (v_len - h_end)
-		continue
+	h_end = find(list(reversed(matrix)), n_diffs)
+	if h_end: return 100 * (v_len - h_end)
 
-	v_start = find(transposed(pattern))
-	if v_start:
-		summary += v_start
-		continue
+	v_start = find(transposed(matrix), n_diffs)
+	if v_start: return v_start
 
-	v_end = find(rev(transposed(pattern)))
-	if v_end:
-		h_len = len(pattern[0])
-		summary += h_len - v_end
-		continue
+	v_end = find(list(reversed(transposed(matrix))), n_diffs)
+	if v_end: return h_len - v_end
 
-	raise Exception('No match found.')
+	raise Exception(f'No mirror image found with {n_diffs} differences.')
 
-print(f'The answer to part 1 is: {summary}.')
+def run(patterns: Matrix, *, n_diffs: int) -> int:
+	return sum([get_value(pattern, n_diffs) for pattern in patterns])
+
+if __name__ == '__main__':
+	patterns = [
+		x.split('\n') for x in open('inputs/day_13.txt').read().split('\n\n')
+	]
+	print(f'The answer to part 1 is: {run(patterns, n_diffs=0)}.')
+	print(f'The answer to part 2 is: {run(patterns, n_diffs=1)}.')
