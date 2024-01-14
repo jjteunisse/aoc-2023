6a34813a2e127ac6ad874303c30015dfd6ab3228
jjteunisse <joristeunisse@gmail.com>
2024-01-14 12:14:55 +0100

    [d16] eerste refactor

--- joris/day_16.py
+++ joris/day_16.py
@@ -1,69 +1,80 @@
+Dims = tuple[int]
 Grid = list[str]
-Tile = list[int, int, str]
+Tile = tuple[int, int, str]
 
-def get_entrances(grid: Grid) -> list[Tile]:
-	y_max, x_max = len(grid), len(grid[0])
+def get_entrances(dims: Dims) -> list[Tile]:
 	return (
-		[[y, 0, '>'] for y in range(0, y_max)] +
-		[[y, x_max - 1, '<'] for y in range(0, y_max)] +
-		[[0, x, 'v'] for x in range(x_max)] +
-		[[y_max - 1, x, '^'] for x in range(x_max)]
+		[(y, 0, '>') for y in range(0, dims[0])] +
+		[(y, dims[1] - 1, '<') for y in range(0, dims[0])] +
+		[(0, x, 'v') for x in range(dims[1])] +
+		[(dims[0] - 1, x, '^') for x in range(dims[1])]
 	)
+
+def init_done(dims: Dims) -> dict[Tile: bool]:
+	return {
+		(y, x, d): False
+		for y in range(dims[0])
+		for x in range(dims[1])
+		for d in ['>', '<', 'v', '^']
+	}
 
 def step(tile: Tile) -> Tile:
 	y, x, d = tile
-	if   d == '^': return [y - 1, x, d]
-	elif d == 'v': return [y + 1, x, d]
-	elif d == '>': return [y, x + 1, d]
-	elif d == '<': return [y, x - 1, d]
+	if   d == '>': return (y, x + 1, d)
+	elif d == '<': return (y, x - 1, d)
+	elif d == 'v': return (y + 1, x, d)
+	elif d == '^': return (y - 1, x, d)
 	else: raise Exception('Unknown direction.')
 
 def turn_split(tile: Tile, grid: Grid) -> list[Tile]:
 	y, x, d = tile
 	match grid[y][x]:
-		case '.': return [[y, x, d]]
+		case '.': return [(y, x, d)]
 		case '/':
-			if   d == '^': return [[y, x, '>']]
-			elif d == 'v': return [[y, x, '<']]
-			elif d == '>': return [[y, x, '^']]
-			elif d == '<': return [[y, x, 'v']]
+			if   d == '>': return [(y, x, '^')]
+			elif d == '<': return [(y, x, 'v')]
+			elif d == 'v': return [(y, x, '<')]
+			elif d == '^': return [(y, x, '>')]
 			else: raise Exception('Unknown direction.')
 		case '\\':
-			if   d == '^': return [[y, x, '<']]
-			elif d == 'v': return [[y, x, '>']]
-			elif d == '>': return [[y, x, 'v']]
-			elif d == '<': return [[y, x, '^']]
+			if   d == '>': return [(y, x, 'v')]
+			elif d == '<': return [(y, x, '^')]
+			elif d == 'v': return [(y, x, '>')]
+			elif d == '^': return [(y, x, '<')]
 			else: raise Exception('Unknown direction.')
 		case '|':
-			if   d in ['>', '<']: return [[y, x, 'v'], [y, x, '^']]
-			elif d in ['^', 'v']: return [[y, x, d]]
+			if   d in ['>', '<']: return [(y, x, 'v'), (y, x, '^')]
+			elif d in ['v', '^']: return [(y, x, d)]
 			else: raise Exception('Unknown direction.')
 		case '-':
-			if   d in ['^', 'v']: return [[y, x, '>'], [y, x, '<']]
-			elif d in ['>', '<']: return [[y, x, d]]
+			if   d in ['>', '<']: return [(y, x, d)]
+			elif d in ['v', '^']: return [(y, x, '>'), (y, x, '<')]
 			else: raise Exception('Unknown direction.')
 		case _:
 			raise Exception('Unknown character.')
 
-def within_bounds(tile: Tile, grid: Grid) -> bool:
-	return 0 <= tile[0] < len(grid) and 0 <= tile[1] < len(grid[0])
+def within_bounds(tile: Tile, dims: Dims) -> bool:
+	return 0 <= tile[0] < dims[0] and 0 <= tile[1] < dims[1]
 
 def run():
 	grid = open('inputs/day_16.txt').read().split('\n')
-	solutions = []
-	for entrance in get_entrances(grid):
-		done, todo = [], [entrance]
+	dims = (len(grid), len(grid[0]))
+	done = init_done(dims)
+	solutions = {}
+	for entrance in get_entrances(dims):
+		done, todo, unique = dict.fromkeys(done, False), [entrance], set()
 		while todo != []:
 			tile = todo.pop(0)
-			done.append(tile)
-			todo += [
-				step(t) for t in turn_split(tile, grid)
-				if within_bounds(step(t), grid) and step(t) not in done
-			]
-		unique_tiles = set(tuple(x[:2]) for x in done)
-		solutions.append((entrance, len(unique_tiles)))
-	print(f'The answer to part 1 is: {solutions[0][1]}.')
-	print(f'The answer to part 2 is: {max(x[1] for x in solutions)}.')
+			done[tile] = True
+			unique.add(tile[:2])
+			for next_tile in turn_split(tile, grid):
+				next_tile = step(next_tile)
+				if within_bounds(next_tile, dims):
+					if not done[next_tile]:
+						todo.append(next_tile)
+		solutions[entrance] = len(unique)
+	print(f'The answer to part 1 is: {solutions[(0, 0, ">")]}.')
+	print(f'The answer to part 2 is: {max(solutions.values())}.')
 
 if __name__ == '__main__':
 	run()
